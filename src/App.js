c0e3a24c0605637710c0ab61a51491ccd3e17433
Guilherme <guilhermekoerich@gmail.com>
2020-03-25 18:38:15 -0300

    Logon and Register Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+import './global.css'
+
+import Routes from './routes'
+
+export default function App() {
   return (
-    <h1>Hello World!</h1>
-  );
+    <Routes />
+  )
 }
-
-export default App;
